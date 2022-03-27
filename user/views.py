d191b9454796b916022ff04f6ca4618b468f2435
Nikhil433-dell <82225430+Nikhil433-dell@users.noreply.github.com>
2022-03-27 12:06:57 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -200,37 +200,37 @@ def bookap(request, id):
 
         service = Create_Service(SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-        # try:
-        #     event = {
-        #         'summary': f'Appointment request for {speciality}',
-        #         'location': '800 Howard St., San Francisco, CA 94103',
-        #         'start': {
-        #             'dateTime': f'{dateap}T09:00:{start_time}:00',
-        #             'timeZone': 'GMT',
-        #         },
-        #         'end': {
-        #             'dateTime': f'{dateap}T17:00:{end_time}:00',
-        #             'timeZone': 'GMT',
-        #         },
-        #         'recurrence': [
-        #             'RRULE:FREQ=DAILY;COUNT=2'
-        #         ],
-        #         'attendees': [
-        #             {'email': 'lpage@example.com'},
-        #             {'email': 'sbrin@example.com'},
-        #         ],
-        #         'reminders': {
-        #             'useDefault': False,
-        #             'overrides': [
-        #             {'method': 'email', 'minutes': 24 * 60},
-        #             {'method': 'popup', 'minutes': 10},
-        #             ],
-        #         },
-        #         }
-        #     event = service.events().insert(calendarId='primary', body=event).execute()
-        #     print(f"event created: {event}")
-        # except Exception as e:
-        #     print(e)
+         try:
+             event = {
+                 'summary': f'Appointment request for {speciality}',
+                 'location': '800 Howard St., San Francisco, CA 94103',
+                 'start': {
+                     'dateTime': f'{dateap}T09:00:{start_time}:00',
+                     'timeZone': 'GMT',
+                 },
+                 'end': {
+                     'dateTime': f'{dateap}T17:00:{end_time}:00',
+                     'timeZone': 'GMT',
+                 },
+                 'recurrence': [
+                     'RRULE:FREQ=DAILY;COUNT=2'
+                 ],
+                 'attendees': [
+                     {'email': 'lpage@example.com'},
+                     {'email': 'sbrin@example.com'},
+                 ],
+                 'reminders': {
+                     'useDefault': False,
+                     'overrides': [
+                     {'method': 'email', 'minutes': 24 * 60},
+                     {'method': 'popup', 'minutes': 10},
+                     ],
+                 },
+                 }
+             event = service.events().insert(calendarId='primary', body=event).execute()
+             print(f"event created: {event}")
+         except Exception as e:
+             print(e)
 
 
 
